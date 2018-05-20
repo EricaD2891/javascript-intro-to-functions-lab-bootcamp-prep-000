312b8e0ab36506ffa235119e15ac1105ea41c24c
Erica Droz <ericad2891@gmail.com>
2018-05-20 01:54:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
     } else 
    {return 'YES INDEED!'}
-
+if (string==string.toUpperCase()){
+  return 'YES INDEED!'
+}
 if (string == "I love you, Grandma."){
   return "I love you, too."
-
-
-
 }
-    }
+}
   
